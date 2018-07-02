51ca3f9fc045aa4844bd5bb894b4575c39cc3b9e
Kavinda Jayakody <kavindajayakody@gmail.com>
2018-07-02 13:21:47 +0530

    Completed component structure

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+// import { HeroesComponent }      from './heroes/heroes.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: []
+})
+export class AppRoutingModule {
+
+  // const routes: Routes = [
+  //   { path: 'heroes', component: HeroesComponent }
+  // ];
+
+}
