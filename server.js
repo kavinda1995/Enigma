64ae429ab0301b684587c8603f6cc30a27fc3af2
Kavinda Jayakody <kavindajayakody@gmail.com>
2018-08-19 00:03:19 +0530

    updates to deploy to heroku (edited basepath) 3

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static('./dist'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join('./dist/index.html'));
+res.sendFile('./dist/index.html');
 });
 
 // Start the app by listening on the default Heroku port
