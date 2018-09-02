988184e18a5f4bf583e6e1c314b1535016076f03
Kavinda Jayakody <kavindajayakody@gmail.com>
2018-09-02 11:37:39 +0530

    added edited frontend

---
+++ frontend/src/app/user/components/login/login.component.ts
@@ -0,0 +1,71 @@
+import { AuthService } from './../../../shared/services/auth.service';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { User } from 'firebase/app';
+import { SharedDataService } from '../../../shared/services/shared-data.service';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  loginForm = true;
+
+  user = {
+    username: '',
+    password: '',
+    cpassword: '',
+    email: ''
+  };
+
+  usernameFormControl = new FormControl('', [
+    Validators.required
+  ]);
+
+  matcher = new MyErrorStateMatcher();
+
+  constructor(private auth: AuthService,
+              private http: HttpClient,
+              private shared: SharedDataService) { }
+
+  ngOnInit() {
+  }
+
+  signInWithGoogle() {
+    this.auth.signInWithGoogle();
+  }
+
+  openRegister() {
+    this.loginForm = false;
+  }
+
+  openLogin() {
+    this.loginForm = true;
+  }
+
+  login() {
+    // login
+    const userData = {
+      username: this.user.username,
+      password: this.user.password
+    };
+
+    this.http.post('http://localhost:3000/user/login', userData).subscribe(data => {
+      console.log(data);
+      this.shared.userSecret = data;
+    });
+  }
+
+}
---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,19 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+};
+
+export const firebase = {
+  apiKey: "<API_KEY>",
+  authDomain: "<PROJECT_ID>.firebaseapp.com",
+  databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
+  storageBucket: "<BUCKET>.appspot.com",
+  messagingSenderId: "<SENDER_ID>",
+};
+
+// set this to true to use static data from "data" folder
+export const staticData = true;
